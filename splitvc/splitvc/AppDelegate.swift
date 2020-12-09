0a63e898d0f6df9179c50a94b5862dc97489dc9f
llscsrl <60774528+llscsrl@users.noreply.github.com>
2020-12-09 19:52:02 +0900

    Add files via upload

---
+++ splitvc/splitvc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  splitvc
+//
+//  Created by tsuyoshi on 2020/12/09.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ splitvc/splitvc/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  splitvc
+//
+//  Created by tsuyoshi on 2020/12/09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
